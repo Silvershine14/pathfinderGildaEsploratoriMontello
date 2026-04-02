8c74e2b6b9c66d613575b4ca4658bb14e97b1dbf
Silvershine14 <94321436+Silvershine14@users.noreply.github.com>
2026-04-02 19:10:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-// Importa Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Inizializza Firebase (usa le credenziali che hai ottenuto)
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyB-2yDPXPOoGGjpbV44VfJYkNRfnezNUkE",
     authDomain: "gildaesploratorimontello.firebaseapp.com",
@@ -14,9 +17,10 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase
     measurementId: "G-KEWSJWLKX2"
   };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 
 // =====================================================
